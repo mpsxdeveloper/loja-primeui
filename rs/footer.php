59d2625cce16716fd1412cf96167ab4d37d4bc52
mpsxdeveloper <98715891+mpsxdeveloper@users.noreply.github.com>
2023-01-20 22:36:01 -0400

    Create footer.php

---
+++ rs/footer.php
@@ -0,0 +1,6 @@
+<div id="footer" title="Loja - <?= date('Y') ?>" style="text-align: center; clear: both;"></div>
+<script>
+    $(document).ready(function() { 
+        $('#footer').puipanel();
+    });
+</script>
